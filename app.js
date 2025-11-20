7f2282ecc9b1c80df0eea727b8a37340735c30dd
JulianaMunoz <jumemu2002@hotmail.com>
2025-11-19 20:33:10 -0500

    intenato arreglar login path desde index ver 3

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       userButton.textContent = "Iniciar sesión";
       // Nota: el HTML ya llama handleUserButton() con onclick, así que esto es redundante pero seguro
-      userButton.onclick = () => window.location.href = "./Usuario/login_usuario.html";
+      userButton.onclick = () => window.location.href = "./Usuario/login_usuario.html"; 
     }
   }
 });
